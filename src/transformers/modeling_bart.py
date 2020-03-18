7629d42432ae9b62bd4ffdc7fbe7554a0c05154c
sshleifer <sshleifer@gmail.com>
2020-03-18 00:05:28 -0400

    cant be worse

--- src/transformers/modeling_bart.py
+++ src/transformers/modeling_bart.py
@@ -290,8 +290,7 @@ class BartEncoder(nn.Module, LoggingMixin):
         # check attention mask and invert
         if attention_mask is not None:
             assert attention_mask.dim() == 2
-
-            attention_mask = (1.0 - attention_mask.long()).long()
+            attention_mask = (1 - attention_mask.long()).long()
             #assert attention_mask.max() <= 0
         inputs_embeds = self.embed_tokens(input_ids)
         embed_pos = self.embed_positions(input_ids)
