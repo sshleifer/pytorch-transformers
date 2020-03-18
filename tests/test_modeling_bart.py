c61f3e4bde2470a40dddfe70a789deab3cb67af8
sshleifer <sshleifer@gmail.com>
2020-03-17 23:26:27 -0400

    del trace

--- tests/test_modeling_bart.py
+++ tests/test_modeling_bart.py
@@ -419,12 +419,12 @@ class BartModelIntegrationTest(unittest.TestCase):
         model = AutoModelForSequenceClassification.from_pretrained("bart-large-mnli").to(torch_device)
         inputs_dict = prepare_bart_inputs_dict(model.config, input_ids)
         # Test that model hasn't changed
-        trace = start_memory_tracing(modules_to_trace="transformers")
+        #trace = start_memory_tracing(modules_to_trace="transformers")
 
         with torch.no_grad():
             batched_logits, features = model.forward(**inputs_dict)
-        summary = MemoryViewer(stop_memory_tracing(trace))
-        summary.save_line_by_line('hf_mem.txt')
+        #summary = MemoryViewer(stop_memory_tracing(trace))
+        #summary.save_line_by_line('hf_mem.txt')
         expected_shape = torch.Size((2, 3))
         self.assertEqual(batched_logits.shape, expected_shape)
         expected_slice = torch.Tensor([[0.1907, 1.4342, -1.0289]]).to(torch_device)
