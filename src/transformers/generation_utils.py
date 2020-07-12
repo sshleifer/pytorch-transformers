cf1ccfd8e2b918360f675d247fb00e186b429db0
Sam Shleifer <sshleifer@gmail.com>
2020-07-12 14:53:19 -0400

    rm print

--- src/transformers/generation_utils.py
+++ src/transformers/generation_utils.py
@@ -643,7 +643,7 @@ class GenerationMixin:
             outputs = self(**model_inputs)  # (batch_size * num_beams, cur_len, vocab_size)
 
             next_token_logits = outputs[0][:, -1, :]  # (batch_size * num_beams, vocab_size)
-            print(f'before softmax: {next_token_logits[0, 1384]}')
+            #print(f'before softmax: {next_token_logits[0, 0]}')
             # if model has past, then set the past variable to speed up decoding
             if self._use_cache(outputs, use_cache):
                 past = outputs[1]
