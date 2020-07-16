0709915c7a967294cd1a0e832a9b04f69711565e
Sam Shleifer <sshleifer@gmail.com>
2020-07-16 22:43:08 +0000

    new pars

--- examples/seq2seq/train_mbart_cc25_enro_multigpu.sh
+++ examples/seq2seq/train_mbart_cc25_enro_multigpu.sh
@@ -6,14 +6,13 @@ python finetune.py \
     --fp16 \
     --gpus $N_GPUS \
     --do_train \
-    --val_check_interval 0.1 \
-    --n_val 500 \
+    --val_check_interval 0.25 \
     --adam_eps 1e-06 \
     --num_train_epochs 6 --src_lang en_XX --tgt_lang ro_RO \
     --data_dir $ENRO_DIR \
     --max_source_length $MAX_LEN --max_target_length $MAX_LEN --val_max_target_length $MAX_LEN --test_max_target_length $MAX_LEN \
     --train_batch_size=$BS --eval_batch_size=$BS --gradient_accumulation_steps=$GAS \
-    --model_name_or_path facebook/mbart-large-cc25 \
+    --tokenizer facebook/mbart-large-cc25 \
     --task translation \
     --warmup_steps 500 --freeze_encoder --freeze_embeds \
     $@
