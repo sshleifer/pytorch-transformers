708b515b989a7bb6a537caa6a3e9a943b3cca6f7
Sam Shleifer <sshleifer@gmail.com>
2020-07-19 18:26:00 +0000

    slightly better train params.

--- examples/seq2seq/train_distilbart_enro.sh
+++ examples/seq2seq/train_distilbart_enro.sh
@@ -5,13 +5,12 @@ python distillation.py \
   --do_train \
   --do_predict \
   --fp16 \
-  --val_check_interval 0.1 \
+  --val_check_interval 0.25 \
   --teacher facebook/mbart-large-en-ro \
   --freeze_encoder --freeze_embeds --data_dir $ENRO_DIR \
   --max_source_length $MAX_LEN --max_target_length $MAX_LEN --val_max_target_length $MAX_LEN --test_max_target_length $MAX_LEN \
-   --student_encoder_layers 12 \
   --freeze_encoder --freeze_embeds \
-  --train_batch_size=$BS --eval_batch_size=$BS --gradient_accumulation_steps=$GAS --num_train_epochs=6 \
+  --train_batch_size=$BS --eval_batch_size=$BS --gradient_accumulation_steps=$GAS --num_train_epochs=3 \
   --tokenizer_name facebook/mbart-large-cc25 --src_lang en_XX --tgt_lang ro_RO \
   --task translation \
   --warmup_steps 500 \
