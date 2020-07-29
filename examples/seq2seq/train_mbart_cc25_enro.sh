b22b88a3102567f3aa09c75b429e0e6059e22847
Sam Shleifer <sshleifer@gmail.com>
2020-07-29 16:33:53 +0000

    6 epochs default

--- examples/seq2seq/train_mbart_cc25_enro.sh
+++ examples/seq2seq/train_mbart_cc25_enro.sh
@@ -9,7 +9,7 @@ python finetune.py \
     --do_predict \
     --val_check_interval 0.1 \
     --adam_eps 1e-06 \
-    --num_train_epochs 3 --src_lang en_XX --tgt_lang ro_RO \
+    --num_train_epochs 6  --src_lang en_XX --tgt_lang ro_RO \
     --data_dir $ENRO_DIR \
     --max_source_length $MAX_LEN --max_target_length $MAX_LEN --val_max_target_length $MAX_LEN --test_max_target_length $MAX_LEN \
     --train_batch_size=$BS --eval_batch_size=$BS --gradient_accumulation_steps=$GAS \
