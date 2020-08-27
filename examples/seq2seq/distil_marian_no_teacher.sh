9b3a844d4844df4bf489b8c815a9c2c381ce6a73
Sam Shleifer <sshleifer@gmail.com>
2020-08-27 05:11:04 +0000

    no_epochs

--- examples/seq2seq/distil_marian_no_teacher.sh
+++ examples/seq2seq/distil_marian_no_teacher.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 export PYTHONPATH="../":"${PYTHONPATH}"
 export WANDB_PROJECT=dmar
-# export MAX_LEN=128
 python distillation.py \
   --learning_rate=3e-4 \
   --do_train \
@@ -11,7 +10,7 @@ python distillation.py \
   --data_dir $ENRO_DIR \
   --max_source_length $MAX_LEN --max_target_length $MAX_LEN --val_max_target_length $MAX_LEN --test_max_target_length $MAX_LEN \
   --freeze_encoder --freeze_embeds \
-  --train_batch_size=$BS --eval_batch_size=$BS --num_train_epochs=6 \
+  --train_batch_size=$BS --eval_batch_size=$BS \
   --tokenizer_name $m --model_name_or_path $m \
   --warmup_steps 500 --sortish_sampler --logger_name wandb \
   --gpus 1 --fp16_opt_level O1 --task translation \
