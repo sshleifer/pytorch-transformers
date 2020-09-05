85e7b7c9249dbc848427b92e3c5ff111596705d6
Sam Shleifer <sshleifer@gmail.com>
2020-09-05 16:14:36 -0400

    just self attn

---
+++ examples/seq2seq/attn_sup_xsum.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+export PYTHONPATH="../":"${PYTHONPATH}"
+export BS=24
+export GAS=2
+export WANDB_PROJECT=transformers_fork-examples_summarization_bart
+python distillation.py \
+  --learning_rate=3e-4 \
+  --do_train \
+  --do_predict \
+  --fp16 \
+  --val_check_interval 0.2 --n_val 1000 --eval_beams 2 \
+  --teacher facebook/bart-large-xsum \
+  --max_target_length=60 --val_max_target_length=60 --test_max_target_length=100 \
+  --student_decoder_layers 3 --student_encoder_layers 12 \
+  --freeze_encoder --freeze_embeds \
+  --model_name_or_path IGNORED \
+  --length_penalty=0.5 \ 
+  --train_batch_size=$BS --eval_batch_size=$BS --gradient_accumulation_steps=$GAS  --num_train_epochs=4 \
+  --tokenizer_name facebook/bart-large --label_smoothing 0.1 \
+  --warmup_steps 500 --logger_name wandb --sortish_sampler --gpus 1 \
+  "$@"
