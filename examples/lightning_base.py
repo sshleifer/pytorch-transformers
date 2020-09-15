d501da779146a2a8ee2e365464d130dae9dd760f
Sam Shleifer <sshleifer@gmail.com>
2020-09-15 14:06:28 -0400

    Decay all params incl layernorm

--- examples/lightning_base.py
+++ examples/lightning_base.py
@@ -127,15 +127,10 @@ class BaseTransformer(pl.LightningModule):
     def configure_optimizers(self):
         """Prepare optimizer and schedule (linear warmup and decay)"""
         model = self.model
-        no_decay = ["bias", "LayerNorm.weight"]
         optimizer_grouped_parameters = [
             {
-                "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+                "params": [p for n, p in model.named_parameters()],
                 "weight_decay": self.hparams.weight_decay,
-            },
-            {
-                "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
-                "weight_decay": 0.0,
             },
         ]
         if self.hparams.adafactor:
