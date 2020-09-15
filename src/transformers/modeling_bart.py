a626a0da6c965f0933a53bde02e30ff050d44552
Sam Shleifer <sshleifer@gmail.com>
2020-09-15 13:33:00 -0400

    boom boom

--- src/transformers/modeling_bart.py
+++ src/transformers/modeling_bart.py
@@ -381,7 +381,7 @@ class BartEncoder(nn.Module):
             else:
 
                 next_x, attn = encoder_layer(x, attention_mask, output_attentions=output_attentions)
-                if not torch.isnan(next_x).any() or torch.isinf(x).any():
+                if not torch.isnan(next_x).any() or torch.isinf(next_x).any():
                     x = next_x
 
             if output_attentions:
