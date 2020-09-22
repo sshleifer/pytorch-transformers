629c684d4a058dbcaa8cb881ab26ad5c510c95d4
Sam Shleifer <sshleifer@gmail.com>
2020-09-21 20:42:07 -0400

    new fix

--- examples/seq2seq/callbacks.py
+++ examples/seq2seq/callbacks.py
@@ -88,7 +88,7 @@ def get_checkpoint_callback(output_dir, metric, save_top_k=1, lower_is_better=Fa
             f"seq2seq callbacks only support rouge2, bleu and loss, got {metric}, You can make your own by adding to this function."
         )
 
-    checkpoint_callback = ModelCheckpoint(
+    checkpoint_callback = FixedModelCheckpoint(
         filepath=os.path.join(output_dir, exp),
         monitor=f"val_{metric}",
         mode="min" if "loss" in metric else "max",
@@ -96,6 +96,64 @@ def get_checkpoint_callback(output_dir, metric, save_top_k=1, lower_is_better=Fa
         period=0,  # maybe save a checkpoint every time val is run, not just end of epoch.
     )
     return checkpoint_callback
+
+from pytorch_lightning.utilities import rank_zero_warn
+class FixedModelCheckpoint(ModelCheckpoint):
+
+    @rank_zero_only
+    def on_validation_end(self, trainer, pl_module):
+        # only run on main process
+        if trainer.global_rank != 0:
+            return
+
+        metrics = pl_module.metrics['val'][-1]
+        epoch = trainer.current_epoch
+        if self.save_top_k == 0:
+            # no models are saved
+            return
+        if self.epoch_last_check is not None and (epoch - self.epoch_last_check) < self.period:
+            # skipping in this term
+            return
+
+        self.epoch_last_check = epoch
+
+        if self.save_last:
+            filepath = os.path.join(self.dirpath, self.prefix + 'last.ckpt')
+            self._save_model(filepath)
+
+        filepath = self.format_checkpoint_name(epoch, metrics)
+        version_cnt = 0
+        while os.path.isfile(filepath):
+            filepath = self.format_checkpoint_name(epoch, metrics, ver=version_cnt)
+            # this epoch called before
+            version_cnt += 1
+
+        if self.save_top_k != -1:
+            current = metrics.get(self.monitor)
+
+            if not isinstance(current, torch.Tensor):
+                rank_zero_warn(
+                    f'The metric you returned {current} must be a `torch.Tensor` instance, checkpoint not saved'
+                    f' HINT: what is the value of {self.monitor} in validation_epoch_end()?', RuntimeWarning
+                )
+                if current is not None:
+                    current = torch.tensor(current)
+
+            if current is None:
+                rank_zero_warn(
+                    f'Can save best model only with {self.monitor} available, skipping.', RuntimeWarning
+                )
+            elif self.check_monitor_top_k(current):
+                self._do_check_save(filepath, current, epoch)
+            elif self.verbose > 0:
+                print(f'\nEpoch {epoch:05d}: {self.monitor}  was not in top {self.save_top_k}')
+
+        else:
+            if self.verbose > 0:
+                print(f'\nEpoch {epoch:05d}: saving model to {filepath}')
+
+            assert trainer.global_rank == 0, 'tried to make a checkpoint from non global_rank=0'
+            self._save_model(filepath)
 
 
 def get_early_stopping_callback(metric, patience):
